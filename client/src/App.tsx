45873edda36647c1b440a49d7cfdc559e0927812
Herb-1 <Shabbydream01@gmail.com>
2025-04-18 00:50:44 +0700

    Fix: Update Footer import in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import Footer from "./components/Footer";
+import { Footer } from "./components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/hooks/use-toast";
 
