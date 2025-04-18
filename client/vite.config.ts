649d938f55821ad1fc39b5ce635936c0af212df4
Herb-1 <Shabbydream01@gmail.com>
2025-04-18 12:24:57 +0700

    Simplify build configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 5173,
-    host: true
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  publicDir: 'public'
+    outDir: 'dist'
+  }
 }) 
